192f6640998da9a414caae1d77866e4be5f1b26d
yphanikumar <yphanikumar@2ac5443b-6a50-0410-97c3-cf255f13d073>
2009-12-09 14:29:53 +0000

    update the TableListener to ClickHandler in GenericTable

---
+++ yalamanchili-gwt/src/main/java/info/yalamanchili/gwt/widgets/Command.java
@@ -0,0 +1,8 @@
+package info.yalamanchili.gwt.widgets;
+
+public abstract class Command implements com.google.gwt.user.client.Command {
+
+	@Override
+	public abstract void execute();
+
+}
--- yalamanchili-gwt/src/main/java/info/yalamanchili/gwt/widgets/GenericTable.java
+++ yalamanchili-gwt/src/main/java/info/yalamanchili/gwt/widgets/GenericTable.java
@@ -1,9 +1,9 @@
 package info.yalamanchili.gwt.widgets;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.SourcesTableEvents;
-import com.google.gwt.user.client.ui.TableListener;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -11,14 +11,14 @@ import com.google.gwt.user.client.ui.Widget;
 /**
  * The Class GenericTable.
  */
-public abstract class GenericTable extends Composite implements TableListener {
+public abstract class GenericTable extends Composite implements ClickHandler {
 
 	/** The panel. */
 	protected VerticalPanel panel = new VerticalPanel();
-	
+
 	/** The paging bar. */
 	protected PagingBar pagingBar = new PagingBar();
-	
+
 	/** The table. */
 	protected FlexTable table = new FlexTable();
 
@@ -42,7 +42,7 @@ public abstract class GenericTable extends Composite implements TableListener {
 	 * Inits the table.
 	 */
 	protected void initTable() {
-		table.addTableListener(this);
+		table.addClickHandler(this);
 		panel.add(table);
 		panel.add(pagingBar);
 	}
@@ -62,25 +62,27 @@ public abstract class GenericTable extends Composite implements TableListener {
 	 */
 	protected abstract void configure();
 
-	/* (non-Javadoc)
-	 * @see com.google.gwt.user.client.ui.TableListener#onCellClicked(com.google.gwt.user.client.ui.SourcesTableEvents, int, int)
-	 */
-	public void onCellClicked(SourcesTableEvents arg0, int row, int col) {
-		viewClicked(row, col);
+	public void onClick(ClickEvent event) {
+		if (event.getSource().equals(table))
+		viewClicked(table.getCellForEvent(event).getRowIndex(), table
+				.getCellForEvent(event).getCellIndex());
 	}
 
 	/**
 	 * View clicked.
 	 * 
-	 * @param row the row
-	 * @param col the col
+	 * @param row
+	 *            the row
+	 * @param col
+	 *            the col
 	 */
 	public abstract void viewClicked(int row, int col);
 
 	/**
 	 * On click.
 	 * 
-	 * @param arg0 the arg0
+	 * @param arg0
+	 *            the arg0
 	 */
 	public void onClick(Widget arg0) {
 		// TODO Auto-generated method stub
