fe03d2b61f3c965e45465616a6bfb92bb8d555d9
yphanikumar <yphanikumar@2ac5443b-6a50-0410-97c3-cf255f13d073>
2010-08-20 19:10:12 +0000

    added httputils support for android/http

---
+++ yalamanchili-android/src/main/java/info/yalamanchili/android/http/HttpHelper.java
@@ -0,0 +1,56 @@
+package info.yalamanchili.android.http;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.thoughtworks.xstream.XStream;
+
+public class HttpHelper {
+	protected static DefaultHttpClient httpclient;
+	public static XStream xstream = new XStream();
+
+	public static DefaultHttpClient getHttpClient() {
+		if (httpclient == null) {
+			httpclient = new DefaultHttpClient();
+			return httpclient;
+		} else {
+			return httpclient;
+		}
+	}
+
+	public static void setCredentials(String username, String password) {
+		getHttpClient().getCredentialsProvider().setCredentials(
+				new AuthScope(null, -1),
+				new UsernamePasswordCredentials(username, password));
+	}
+
+	public static String request(HttpResponse response) {
+		String result = "";
+		try {
+			InputStream in = response.getEntity().getContent();
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(in));
+			StringBuilder str = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				str.append(line + "\n");
+			}
+			in.close();
+			result = str.toString();
+		} catch (Exception e) {
+			result = "Error";
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+
+	public static Boolean serverStateCheck() {
+		return true;
+	}
+}
