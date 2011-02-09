f7895c5ba1ac644a802809e2c9e82b56713660f5
yphanikumar <yphanikumar@2ac5443b-6a50-0410-97c3-cf255f13d073>
2011-02-09 21:41:00 +0000

    added AbstractEntity support

---
+++ yalamanchili-commons/src/main/java/info/yalamanchili/gwt/jpa/AbstractEntity.java
@@ -0,0 +1,48 @@
+package info.yalamanchili.gwt.jpa;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+import net.sf.gilead.pojo.gwt.LightEntity;
+
+/**
+ * Default Parent/root entity for all jpa entities to serve id,version
+ * properties and extends from LightEntity to suppport GWT-GILEAD
+ * 
+ * @author ayalamanchili
+ */
+@XmlType
+@MappedSuperclass
+public class AbstractEntity extends LightEntity implements Serializable{
+	@Transient
+	public static final long serialVersionUID = 196919661993L;
+
+	@XmlAttribute
+	@Version
+	private Integer version;
+
+	@XmlElement
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+}
