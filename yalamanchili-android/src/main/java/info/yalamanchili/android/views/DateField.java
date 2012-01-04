50a6cd594046a40f513469f2b6680990ad880c0d
yphanikumar@gmail.com <yphanikumar@gmail.com@2ac5443b-6a50-0410-97c3-cf255f13d073>
2012-01-04 17:27:43 +0000

    date field working copy

--- yalamanchili-android/src/main/java/info/yalamanchili/android/views/DateField.java
+++ yalamanchili-android/src/main/java/info/yalamanchili/android/views/DateField.java
@@ -1,19 +1,14 @@
 package info.yalamanchili.android.views;
 
-import java.util.Calendar;
 import java.util.Date;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,40 +16,24 @@ public class DateField extends BaseField {
 
 	protected static final int DATE_ACTIVITY_REQUEST_CODE = 100;
 
-	Calendar date = Calendar.getInstance();
+	protected int dateFieldId;
 
 	protected TextView dateView;
 	protected Button dateButton;
-
-	private int mYear;
-	private int mMonth;
-	private int mDay;
-
-	static final int DATE_DIALOG_ID = 999;
-
-	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-				int dayOfMonth) {
-			date.set(year, monthOfYear, dayOfMonth);
-			updateDisplay();
-		}
-	};
 
 	public DateField(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
 	public void init() {
-		// TODO handle readOnly logic
 		label = new TextView(getContext());
 		dateView = new TextView(getContext());
 		errorMsg = new TextView(getContext());
 		dateButton = new Button(getContext());
 		dateButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				Log.d("----------------", "on onclick-----------------");
 				Intent i = new Intent(getContext(), DateWidget.class);
+				i.putExtra("id", getDateFieldId());
 				((Activity) getContext()).startActivityForResult(i,
 						DATE_ACTIVITY_REQUEST_CODE);
 			}
@@ -71,43 +50,19 @@ public class DateField extends BaseField {
 		addView(dateButton, new LinearLayout.LayoutParams(lWidth, lHeight));
 		addView(errorMsg, new LinearLayout.LayoutParams(lWidth, lHeight));
 		label.setTypeface(Typeface.DEFAULT_BOLD);
-		// get the current date
-		final Calendar c = Calendar.getInstance();
-		mYear = c.get(Calendar.YEAR);
-		mMonth = c.get(Calendar.MONTH);
-		mDay = c.get(Calendar.DAY_OF_MONTH);
-
-		// display the current date (this method is below)
-		updateDisplay();
 	}
 
 	@Override
 	protected void customize() {
-		dateButton.setText("asdf");
-		// TODO Auto-generated method stub
-
+		dateButton.setText("Date");
 	}
-
-	// @Override
-	// protected Dialog onCreateDialog(int id) {
-	// switch (id) {
-	// case DATE_DIALOG_ID:
-	// return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
-	// mDay);
-	// }
-	// return null;
-	// }
 
 	public void setValue(String value) {
 		dateView.setText(value);
 	}
 
-	public String getValue() {
-		return dateView.getText().toString();
-	}
-
-	public Date getDate() {
-		return DateWidget.getDate();
+	public Date getValue() {
+		return DateWidget.getDate(getDateFieldId());
 	}
 
 	public void setLabel(String label) {
@@ -122,22 +77,12 @@ public class DateField extends BaseField {
 		errorMsg.setText(message);
 	}
 
-	private void updateDisplay() {
-		dateView.setText(new StringBuilder()
-				// Month is 0 based so add 1
-				.append(mMonth + 1).append("-").append(mDay).append("-")
-				.append(mYear).append(" "));
+	public int getDateFieldId() {
+		return dateFieldId;
 	}
 
-	public Button getDateButton() {
-		return dateButton;
+	public void setDateFieldId(int dateFieldId) {
+		this.dateFieldId = dateFieldId;
 	}
 
-	public Integer getDateId() {
-		return DATE_DIALOG_ID;
-	}
-
-	public OnDateSetListener getDateSetListener() {
-		return mDateSetListener;
-	}
 }
--- yalamanchili-android/src/main/java/info/yalamanchili/android/views/DateWidget.java
+++ yalamanchili-android/src/main/java/info/yalamanchili/android/views/DateWidget.java
@@ -2,16 +2,21 @@ package info.yalamanchili.android.views;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.DatePicker;
 
 public class DateWidget extends Activity {
 
 	protected static final int SUCCESS_RETURN_CODE = 100;
+	protected static Map<Integer, Date> dates = new HashMap<Integer, Date>();
+
 	public static Calendar date = Calendar.getInstance();
 
 	@Override
@@ -26,18 +31,23 @@ public class DateWidget extends Activity {
 		int cmonth = Calendar.getInstance().get(Calendar.MONTH);
 		int cday = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
 
+		
 		DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
 
 			public void onDateSet(DatePicker view, int year, int monthOfYear,
 					int dayOfMonth) {
 				date.set(year, monthOfYear, dayOfMonth);
+				int id = getIntent().getIntExtra("id", 0);
+				dates.put(id, date.getTime());
 				finish();
 			}
 		};
 		return new DatePickerDialog(this, mDateSetListener, cyear, cmonth, cday);
 	}
 
-	public static Date getDate() {
-		return date.getTime();
+	public static Date getDate(int id) {
+		Date date = dates.get(id);
+		dates.remove(id);
+		return date;
 	}
 }
