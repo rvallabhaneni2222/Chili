4677e23fe4cc49ddce3fbe1a341a953b8b8f19ff
yphanikumar@gmail.com <yphanikumar@gmail.com@2ac5443b-6a50-0410-97c3-cf255f13d073>
2012-08-01 12:50:42 +0000

    validations for dto clientinfo and emergencycontact

---
+++ chili-master/chili-commons/src/main/java/info/chili/service/jrs/exception/ServiceException.java
@@ -0,0 +1,158 @@
+package info.chili.service.jrs.exception;
+
+import info.chili.service.jrs.types.Errors;
+
+import java.util.List;
+
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private int statusCode;
+    private Errors errors;
+
+    public ServiceException(StatusCode statusCode, Errors errors) {
+        this.statusCode = statusCode.value();
+        this.errors = errors;
+    }
+
+    public ServiceException(StatusCode statusCode, String source, String reasonCode) {
+        this(statusCode, new info.chili.service.jrs.types.Error(source, reasonCode));
+    }
+
+    public ServiceException(StatusCode statusCode, String source, String reasonCode, Throwable e) {
+        this(e, statusCode, new info.chili.service.jrs.types.Error(source, reasonCode));
+    }
+
+    public ServiceException(StatusCode statusCode, List<info.chili.service.jrs.types.Error> errs) {
+        this.statusCode = statusCode.value();
+        if (this.errors == null) {
+            this.errors = new Errors();
+        }
+        for (info.chili.service.jrs.types.Error error : errs) {
+            this.errors.addError(error);
+        }
+    }
+
+    public ServiceException(Throwable e, StatusCode statusCode, List<info.chili.service.jrs.types.Error> errs) {
+        super(e);
+        this.statusCode = statusCode.value();
+        if (this.errors == null) {
+            this.errors = new Errors();
+        }
+        for (info.chili.service.jrs.types.Error error : errs) {
+            this.errors.addError(error);
+        }
+    }
+
+    public ServiceException(Throwable e, StatusCode statusCode, info.chili.service.jrs.types.Error... errs) {
+        super(e);
+        this.statusCode = statusCode.value();
+        if (this.errors == null) {
+            this.errors = new Errors();
+        }
+        for (info.chili.service.jrs.types.Error error : errs) {
+            this.errors.addError(error);
+        }
+    }
+
+    public ServiceException(StatusCode statusCode, info.chili.service.jrs.types.Error... errs) {
+        this.statusCode = statusCode.value();
+        if (this.errors == null) {
+            this.errors = new Errors();
+        }
+        for (info.chili.service.jrs.types.Error error : errs) {
+            this.errors.addError(error);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceException [statusCode=" + statusCode + ", errors=" + errors + "]";
+    }
+
+    public Errors getErrors() {
+        return errors;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public enum StatusCode {
+
+        OK(200), NEW_RESOURCE_CREATED(201), INVALID_REQUEST(400), NOT_AUTHORIZED_REQUEST(401), RESOURCE_NOT_FOUND(404), INTERNAL_SYSTEM_ERROR(
+        500);
+        private int statusCode;
+
+        StatusCode(int statusCode) {
+            this.statusCode = statusCode;
+        }
+
+        public int value() {
+            return statusCode;
+        }
+
+        public String toString() {
+            return String.valueOf(statusCode);
+        }
+    }
+
+    public enum ReasonCode {
+
+        /**
+         * The request processing time took too long and it was determined that
+         * request should be timed out.
+         */
+        REQUEST_TIMEOUT,
+        /**
+         * An error that occurs due to a hardware, network, or connectivity
+         * failure.
+         */
+        SYSTEM_ERROR,
+        /**
+         * An error that occurs if a client makes too many requests and exceeds
+         * the throttling setting for a service.
+         */
+        VOLUME_THRESHOLD_EXCEEDED,
+        /**
+         * An error that occurs when the client's request does not match a known
+         * service URL.
+         */
+        URL_UNKNOWN,
+        /**
+         * An error that occurs when the client's OAuth credentials are not
+         * authorized to make the request.
+         */
+        AUTHORIZATION_FAILED,
+        /**
+         * An error that occurs when the client's OAuth credentials are not
+         * successfully validated.
+         */
+        AUTHENTICATION_FAILED,
+        /**
+         * An error that indicated that a piece of data required in the request
+         * was not present.
+         */
+        MISSING_REQUIRED_INPUT,
+        /**
+         * An error that indicated that a peice of data in the request does not
+         * match the expected format or mask.
+         */
+        INVALID_INPUT_FORMAT,
+        /**
+         * An error that indicated that a piece of data in the request is too
+         * long or too short.
+         */
+        INVALID_INPUT_LENGTH,
+        /**
+         * An error that indicates that the value of a piece of data in the
+         * request is invalid.
+         */
+        INVALID_INPUT_VALUE,
+        /**
+         * An error that indicates that a specific resource instance can not be
+         * found.
+         */
+        RESOURCE_UNKNOWN;
+    }
+}
