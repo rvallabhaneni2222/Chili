cde43da16de54d19f5ae2b64933217ec40056484
yphanikumar@gmail.com <yphanikumar@gmail.com@2ac5443b-6a50-0410-97c3-cf255f13d073>
2012-10-21 17:30:33 +0000

    udpate datefield to use org.zenkia date picker.

--- chili-master/chili-gwt/src/main/java/info/chili/gwt/fields/DateField.java
+++ chili-master/chili-gwt/src/main/java/info/chili/gwt/fields/DateField.java
@@ -13,19 +13,15 @@ import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.ConstantsWithLookup;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiConstructor;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.datepicker.client.DatePicker;
+
 
 import info.chili.gwt.date.DateUtils;
-import info.chili.gwt.resources.ChiliImages;
 import java.util.logging.Logger;
+import org.zenika.widget.client.datePicker.DatePicker;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -36,9 +32,7 @@ public class DateField extends BaseField implements KeyPressHandler, KeyUpHandle
     private static Logger logger = Logger.getLogger(DateField.class.getName());
     final TextBox dateField = new TextBox();
     final DatePicker datePicker = new DatePicker();
-    Image datePickerIcon = new Image();
-    PopupPanel popupPanel = new PopupPanel(true);
-    protected Date date = null;
+
 
     public DatePicker getDatePicker() {
         return datePicker;
@@ -52,19 +46,19 @@ public class DateField extends BaseField implements KeyPressHandler, KeyUpHandle
     }
 
     public Date getDate() {
-        return date;
+        return datePicker.getSelectedDate();
     }
 
     public void setDate(Date date) {
         if (date != null) {
             dateField.setText(Utils.getShortDate(date));
-            datePicker.setValue(date);
+            datePicker.setSelectedDate(date);
         }
     }
 
     public void setValue(String dateString) {
         //need to set the datepicker date to support udpate
-        datePicker.setValue(DateUtils.toDate(dateString));
+        datePicker.setSelectedDate(DateUtils.toDate(dateString));
         String formatedDate = DateUtils.getFormatedDate(dateString, DateTimeFormat.PredefinedFormat.DATE_MEDIUM);
         dateField.setText(formatedDate);
     }
@@ -80,20 +74,7 @@ public class DateField extends BaseField implements KeyPressHandler, KeyUpHandle
     protected void configureAddMainWidget() {
         dateField.ensureDebugId(className + "_" + attributeName + "_TB");
         dateField.addKeyPressHandler(this);
-        datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<Date> event) {
-                date = datePicker.getValue();
-                String dateString = DateTimeFormat.getFormat("d MMMM yyyy").format(date);
-                dateField.setText(dateString);
-            }
-        });
-        fieldPanel.insert(dateField, 0);
-        datePickerIcon.addClickHandler(this);
-        datePickerIcon.setResource(ChiliImages.INSTANCE.datePickerIcon());
-        fieldPanel.insert(datePickerIcon, 1);
-        //popup for date picker
-        popupPanel.add(datePicker);
+        fieldPanel.insert(datePicker, 0);
     }
 
     public void onKeyPress(KeyPressEvent event) {
@@ -114,10 +95,6 @@ public class DateField extends BaseField implements KeyPressHandler, KeyUpHandle
 
     @Override
     public void onClick(ClickEvent event) {
-        if (event.getSource().equals(datePickerIcon)) {
-            popupPanel.setPopupPosition(datePickerIcon.getAbsoluteLeft() + datePickerIcon.getOffsetWidth(),
-                    datePickerIcon.getAbsoluteTop() + datePickerIcon.getOffsetHeight());
-            popupPanel.show();
-        }
+       
     }
 }
