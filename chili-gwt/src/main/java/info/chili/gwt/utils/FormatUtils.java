6b029cfc6a562b806e6b11353af0277de8ea1837
yphanikumar@gmail.com <yphanikumar@gmail.com@2ac5443b-6a50-0410-97c3-cf255f13d073>
2013-11-23 14:26:37 +0000

    curency format

--- chili-gwt/src/main/java/info/chili/gwt/utils/FormatUtils.java
+++ chili-gwt/src/main/java/info/chili/gwt/utils/FormatUtils.java
@@ -1,5 +1,8 @@
 package info.chili.gwt.utils;
 
+import com.google.gwt.i18n.client.NumberFormat;
+import java.math.BigDecimal;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -19,4 +22,23 @@ public class FormatUtils {
         }
         return phoneNumber.replaceFirst("^(\\d{3})(\\d{3})(\\d{4})$", "$1-$2-$3");
     }
+
+    public static String formarCurrency(String amount) {
+        BigDecimal amt;
+        try {
+            amt = new BigDecimal(amount);
+        } catch (Exception e) {
+            return "";
+        }
+        return formatCurrency(amt);
+    }
+
+    public static String formatCurrency(BigDecimal amount) {
+        if (amount == null) {
+            return "";
+        }
+        NumberFormat fmt = NumberFormat.getCurrencyFormat();
+        return fmt.format(amount);
+    }
+
 }
