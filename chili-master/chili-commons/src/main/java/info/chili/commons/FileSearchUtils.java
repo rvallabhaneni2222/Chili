826dd4dfb6ab2431c2449fc58c45b41fcc71848e
yphanikumar@gmail.com <yphanikumar@gmail.com@2ac5443b-6a50-0410-97c3-cf255f13d073>
2014-04-14 20:25:05 +0000

    pdf extraction support

--- chili-master/chili-commons/src/main/java/info/chili/commons/FileSearchUtils.java
+++ chili-master/chili-commons/src/main/java/info/chili/commons/FileSearchUtils.java
@@ -5,7 +5,13 @@
  */
 package info.chili.commons;
 
+import com.google.common.io.Files;
 import java.io.File;
+import java.io.FileInputStream;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 import org.apache.poi.extractor.ExtractorFactory;
 
 /**
@@ -15,6 +21,44 @@ import org.apache.poi.extractor.ExtractorFactory;
 public class FileSearchUtils {
 
     public static String extractFileContents(File file) {
+        if (isMSDoc(file)) {
+            return extractMSDocFileContents(file);
+        } else if (isPdfDoc(file)) {
+            return extractPDFFileContents(file);
+        }
+        throw new UnsupportedOperationException("unknown file format cannot extract contents:" + getFileExtension(file));
+    }
+
+    public static boolean isPdfDoc(File file) {
+        if (getFileExtension(file).equalsIgnoreCase("pdf")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * return true for word, excel, and powerpoint
+     *
+     * @param file
+     * @return
+     */
+    public static boolean isMSDoc(File file) {
+        String ext = getFileExtension(file);
+        if (ext.contains("doc") || ext.contains("xls") || ext.contains("ppt")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * extracts Microsoft based document contents
+     *
+     * @param file
+     * @return
+     */
+    public static String extractMSDocFileContents(File file) {
         String content = "";
         try {
             content = ExtractorFactory.createExtractor(file).getText();
@@ -22,5 +66,21 @@ public class FileSearchUtils {
             throw new RuntimeException("error extractiong file contents", e);
         }
         return content;
+    }
+
+    public static String extractPDFFileContents(File file) {
+        try {
+            PDFParser parser = new PDFParser(new FileInputStream(file));
+            parser.parse();
+            COSDocument cd = parser.getDocument();
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(new PDDocument(cd));
+        } catch (Exception e) {
+            throw new RuntimeException("error extractiong PDF file contents", e);
+        }
+    }
+
+    public static String getFileExtension(File file) {
+        return Files.getFileExtension(file.getName());
     }
 }
