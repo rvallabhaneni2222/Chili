d5c041689692dac504ecb1ac7715e30e1aa5d920
anu@yalamanchili.info <>
2015-06-15 11:35:25 -0400

    http://tools.sstech.mobi/jira/browse/SSTO-2308

--- chili-master/chili-commons/src/main/java/info/chili/commons/HtmlUtils.java
+++ chili-master/chili-commons/src/main/java/info/chili/commons/HtmlUtils.java
@@ -5,12 +5,25 @@
  */
 package info.chili.commons;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.w3c.tidy.Tidy;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+import java.util.Locale;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+import org.outerj.daisy.diff.HtmlCleaner;
+import org.outerj.daisy.diff.html.HTMLDiffer;
+import org.outerj.daisy.diff.html.HtmlSaxDiffOutput;
+import org.outerj.daisy.diff.html.TextNodeComparator;
+import org.outerj.daisy.diff.html.dom.DomTreeBuilder;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
 
 /**
  *
@@ -39,5 +52,49 @@ public class HtmlUtils {
     public static String htmlToText(String html) {
         Document doc = Jsoup.parse(html);
         return doc.body().text();
+    }
+
+    public static String getDiffForHTMLInput(String contentOld, String contentNew)
+            throws Exception {
+        contentOld = contentOld == null ? "" : contentOld;
+        contentNew = contentNew == null ? "" : contentNew;
+        StringWriter writer = new StringWriter(contentNew.length()
+                + contentOld.length());
+        //List styleList = new ArrayList();
+        //styleList.add("/static/js/difftag/css/difftag.css");
+
+        SAXTransformerFactory tf = (SAXTransformerFactory) TransformerFactory
+                .newInstance();
+        TransformerHandler result = tf.newTransformerHandler();
+        result.setResult(new StreamResult(writer));
+
+        ContentHandler postProcess = result;
+        Locale locale = Locale.getDefault();
+        String prefix = "diff";
+        HtmlCleaner cleaner = new HtmlCleaner();
+        InputSource oldSource = new InputSource(new ByteArrayInputStream(
+                contentOld.getBytes("UTF-8")));
+        InputSource newSource = new InputSource(new ByteArrayInputStream(
+                contentNew.getBytes("UTF-8")));
+        DomTreeBuilder oldHandler = new DomTreeBuilder();
+        cleaner.cleanAndParse(oldSource, oldHandler);
+        TextNodeComparator leftComparator = new TextNodeComparator(oldHandler,
+                locale);
+        DomTreeBuilder newHandler = new DomTreeBuilder();
+        cleaner.cleanAndParse(newSource, newHandler);
+        TextNodeComparator rightComparator = new TextNodeComparator(newHandler,
+                locale);
+        postProcess.startDocument();
+
+        //postProcess.startElement("", "diffreport", "diffreport", new AttributesImpl());
+        // attachStyleSheets(styleList, postProcess);
+        //postProcess.startElement("", "diff", "diff", new AttributesImpl());
+        HtmlSaxDiffOutput output = new HtmlSaxDiffOutput(postProcess, prefix);
+        HTMLDiffer differ = new HTMLDiffer(output);
+        differ.diff(leftComparator, rightComparator);
+        //postProcess.endElement("", "diff", "diff");
+        //postProcess.endElement("", "diffreport", "diffreport");
+        postProcess.endDocument();
+        return writer.getBuffer().toString();
     }
 }
