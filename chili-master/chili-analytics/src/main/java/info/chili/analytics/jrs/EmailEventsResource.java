864fbc830d7fcf5d8f1d1afc7e6ad9ec5820d1f5
anu@yalamanchili.info <>
2015-08-10 17:54:37 -0400

    email analytics

--- chili-master/chili-analytics/src/main/java/info/chili/analytics/jrs/EmailEventsResource.java
+++ chili-master/chili-analytics/src/main/java/info/chili/analytics/jrs/EmailEventsResource.java
@@ -6,16 +6,15 @@
 package info.chili.analytics.jrs;
 
 import info.chili.analytics.service.EmailEventsService;
-import info.chili.email.Email;
+import info.chili.analytics.service.EmailEventsService.EmailEventDto;
+import info.chili.analytics.service.EmailEventsService.EmailEventsTable;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -40,29 +39,9 @@ public class EmailEventsResource {
         return emailEventsService.getEvents(start, limit);
     }
 
-    @XmlRootElement
-    @XmlType
-    public static class EmailEventsTable implements java.io.Serializable {
-
-        protected Long size;
-        protected List<Email> entities;
-
-        public Long getSize() {
-            return size;
-        }
-
-        public void setSize(Long size) {
-            this.size = size;
-        }
-
-        @XmlElement
-        public List<Email> getEntities() {
-            return entities;
-        }
-
-        public void setEntities(List<Email> entities) {
-            this.entities = entities;
-        }
+    @PUT
+    @Path("/search")
+    public List<EmailEventDto> search(EmailEventDto search) {
+        return emailEventsService.searchEvents(search);
     }
-
 }
--- chili-master/chili-analytics/src/main/java/info/chili/analytics/service/EmailEventsService.java
+++ chili-master/chili-analytics/src/main/java/info/chili/analytics/service/EmailEventsService.java
@@ -6,10 +6,14 @@
 package info.chili.analytics.service;
 
 import com.google.common.base.Strings;
-import info.chili.analytics.jrs.EmailEventsResource.EmailEventsTable;
-import info.chili.analytics.model.Event;
 import info.chili.email.Email;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Sort;
@@ -35,26 +39,111 @@ public class EmailEventsService {
         EmailEventsTable res = new EmailEventsTable();
         Query query = new Query();
         query.with(new Sort(Sort.Direction.DESC, "sentTimeStamp"));
-        res.setEntities(mongoTemplate.find(query.limit(limit).skip(start), Email.class));
+        for (Email email : mongoTemplate.find(query.limit(limit).skip(start), Email.class)) {
+            EmailEventDto dto = new EmailEventDto();
+            dto.setTo(StringUtils.join(email.getTos(), ", "));
+            dto.setSentTimeStamp(email.getSentTimeStamp());
+            dto.setSubject(email.getSubject());
+            dto.setBody(email.getBody());
+            res.getEntities().add(dto);
+        }
         res.setSize(mongoTemplate.count(query, Email.class));
         return res;
     }
 
-    public List<Event> searchEvents(SearchEventDto search) {
+    public List<EmailEventDto> searchEvents(EmailEventDto search) {
+        List<EmailEventDto> res = new ArrayList();
         Query query = new Query();
-        if (!Strings.isNullOrEmpty(search.getUser())) {
-            query.addCriteria(Criteria.where("user").is(search.getUser().trim()));
+        if (!Strings.isNullOrEmpty(search.getTo())) {
+            query.addCriteria(Criteria.where("tos").regex(search.getTo(),"i"));
         }
-        if (!Strings.isNullOrEmpty(search.getName())) {
-            query.addCriteria(Criteria.where("name").regex(search.getName().trim()));
+        if (!Strings.isNullOrEmpty(search.getBody())) {
+            query.addCriteria(Criteria.where("body").regex(search.getBody(),"i"));
         }
-        if (!Strings.isNullOrEmpty(search.getInput())) {
-            query.addCriteria(Criteria.where("input").regex(search.getInput().trim()));
+        if (!Strings.isNullOrEmpty(search.getSubject())) {
+             query.addCriteria(Criteria.where("subject").regex(search.getSubject(),"i"));
+
         }
-        if (!Strings.isNullOrEmpty(search.getOutput())) {
-            query.addCriteria(Criteria.where("output").regex(search.getOutput().trim()));
+        query.with(new Sort(Sort.Direction.DESC, "sentTimeStamp"));
+        for (Email email : mongoTemplate.find(query.limit(100), Email.class)) {
+            EmailEventDto dto = new EmailEventDto();
+            dto.setTo(StringUtils.join(email.getTos(), ", "));
+            dto.setSentTimeStamp(email.getSentTimeStamp());
+            dto.setSubject(email.getSubject());
+            dto.setBody(email.getBody());
+            res.add(dto);
         }
-        query.with(new Sort(Sort.Direction.DESC, "evenTimeStamp"));
-        return mongoTemplate.find(query.limit(100), Event.class);
+        return res;
     }
+
+    @XmlRootElement
+    @XmlType
+    public static class EmailEventDto implements java.io.Serializable {
+
+        protected String to;
+        protected Date sentTimeStamp;
+        protected String subject;
+        protected String body;
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public Date getSentTimeStamp() {
+            return sentTimeStamp;
+        }
+
+        public void setSentTimeStamp(Date sentTimeStamp) {
+            this.sentTimeStamp = sentTimeStamp;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+    }
+
+    @XmlRootElement
+    @XmlType
+    public static class EmailEventsTable implements java.io.Serializable {
+
+        protected Long size;
+        protected List<EmailEventDto> entities;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        @XmlElement
+        public List<EmailEventDto> getEntities() {
+            if (this.entities == null) {
+                this.entities = new ArrayList<>();
+            }
+            return entities;
+        }
+
+        public void setEntities(List<EmailEventDto> entities) {
+            this.entities = entities;
+        }
+    }
+
 }
