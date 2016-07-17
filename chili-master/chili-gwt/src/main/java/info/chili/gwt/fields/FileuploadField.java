972f86e10d5e39cfbbfa2de6b0f45572efbaf2e2
phani.y@sstech.us <>
2016-07-17 10:04:46 -0400

    support for mutiple file upload...

--- chili-master/chili-gwt/src/main/java/info/chili/gwt/fields/FileuploadField.java
+++ chili-master/chili-gwt/src/main/java/info/chili/gwt/fields/FileuploadField.java
@@ -9,6 +9,7 @@ import com.google.gwt.i18n.client.ConstantsWithLookup;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FileUpload;
@@ -25,7 +26,6 @@ import info.chili.gwt.config.ChiliClientConfig;
 import info.chili.gwt.utils.FileUtils;
 import info.chili.gwt.utils.JSONUtils;
 import info.chili.gwt.utils.Utils;
-import info.chili.gwt.widgets.ClickableLink;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -41,10 +41,8 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
     protected FormPanel formPanel = new FormPanel();
     Label label = new Label("upload");
     FileUpload fileUpload = new FileUpload();
-    List<FileUpload> fileUploads = new ArrayList<FileUpload>();
     protected HTML message = new HTML();
     Button submit = new Button("Upload");
-    ClickableLink addMoreFiles = new ClickableLink("add more..");
 
     public FileuploadField(ConstantsWithLookup constants, String className, String attributeName, String filePrefix, boolean required) {
         instance = this;
@@ -53,7 +51,6 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
         this.filePrefix = filePrefix;
         init(formPanel);
         submit.setVisible(false);
-        addMoreFiles.setVisible(false);
     }
 
     public FileuploadField(ConstantsWithLookup constants, String className, String attributeName, String filePrefix, boolean required, boolean supportMultiple) {
@@ -63,7 +60,7 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
         this.filePrefix = filePrefix;
         init(formPanel);
         submit.setVisible(false);
-        addMoreFiles.setVisible(supportMultiple);
+        fileUpload.getElement().setAttribute("multiple", "multiple");
     }
 
     @Override
@@ -79,7 +76,6 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
         formPanel.setMethod(FormPanel.METHOD_POST);
         fileUpload.setName(filePrefix);
         formPanel.setAction(getUploadUrl());
-        addMoreFiles.addClickHandler(this);
     }
 
     protected String getUploadUrl() {
@@ -94,31 +90,18 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
     protected void addWidgets() {
         panel.add(label);
         panel.add(fileUpload);
-        fileUploads.add(fileUpload);
         panel.add(message);
-        panel.add(addMoreFiles);
         panel.add(submit);
         formPanel.add(panel);
     }
 
     @Override
     public void onClick(ClickEvent arg0) {
-        if (arg0.getSource().equals(addMoreFiles)) {
-            addFileUpload();
-            return;
-        }
         clearMessage();
         if (arg0.getSource().equals(submit)) {
             formPanel.submit();
         }
         formPanel.clear();
-    }
-
-    protected void addFileUpload() {
-        FileUpload upload = new FileUpload();
-        upload.setName(filePrefix);
-        fileUploads.add(upload);
-        panel.add(upload);
     }
 
     public void upload(String entityId) {
@@ -173,21 +156,48 @@ public abstract class FileuploadField extends ALComposite implements ClickHandle
 
 //TODO make this better
     public void setEntityIds(JSONArray fileObjs, String fileUrlFieldName) {
+        StringBuilder fileNameB = new StringBuilder();
+        int j = 0;
         for (int i = 0; i < fileObjs.size(); i++) {
             JSONObject fileObj = (JSONObject) fileObjs.get(i);
             String entityId = JSONUtils.toString(fileObj, "id");
             String url = JSONUtils.toString(fileObj, fileUrlFieldName) + "&";
-            for (FileUpload upload : fileUploads) {
-                if (upload.getFilename().replaceAll("[^a-zA-Z0-9\\._]+", "_").contains(FileUtils.getFileNameFromUrl(url))) {
-                    upload.setName(filePrefix + "_" + entityId + "_");
-                }
+            if (getFileNames().get(j).stringValue().replaceAll("[^a-zA-Z0-9\\._]+", "_").contains(FileUtils.getFileNameFromUrl(url))) {
+                String name = filePrefix + "_" + entityId + "_";
+                fileNameB.append(name).append(";");
+                j++;
+
             }
         }
+        fileUpload.setName(fileNameB.toString());
     }
 
-    public List<FileUpload> getFileUploads() {
-        return fileUploads;
+    public List<JSONString> getFileNames() {
+        List<JSONString> files = new ArrayList<JSONString>();
+        for (String str : getFileNames(fileUpload.getElement()).split(";")) {
+            files.add(new JSONString(filePrefix + "_entityId_" + stripFileName(str)));
+        }
+        return files;
     }
+
+    public static native String getFileNames(Element input) /*-{
+
+    var ret = "";
+
+    //microsoft support
+    if (typeof (input.files) == 'undefined'
+            || typeof (input.files.length) == 'undefined') {
+        return input.value;
+    }
+
+    for ( var i = 0; i < input.files.length; i++) {
+        if (i > 0) {
+            ret += ";";
+        }
+        ret += input.files[i].name;
+    }
+    return ret;
+}-*/;
 
     public FileUpload getFileUpload() {
         return fileUpload;
