2ce53c492310af9014428a8f6200467e0132c4bf
Phani Yalamanchili <phani.y@sstech.us>
2016-08-08 16:42:32 -0400

    TReadComposite support for back button

--- chili-master/chili-gwt/src/main/java/info/chili/gwt/crud/TReadComposite.java
+++ chili-master/chili-gwt/src/main/java/info/chili/gwt/crud/TReadComposite.java
@@ -9,7 +9,10 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.ConstantsWithLookup;
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  *
@@ -18,37 +21,35 @@ import com.google.gwt.user.client.ui.Button;
 public abstract class TReadComposite extends TCRUDComposite {
 
     protected Button cloneB = new Button("Create Copy");
+    Anchor backL = new Anchor("Back");
 
     protected void initReadComposite(JSONObject entity, String className, final ConstantsWithLookup constants) {
         this.entity = entity;
+        configureBack();
         init(className, true, constants);
-        entityCaptionPanel.addStyleName("y-gwt-ReadEntityCaptionPanel");
-        entityFieldsPanel.addStyleName("y-gwt-ReadEntityDisplayWidget");
-        basePanel.addStyleName("y-gwt-ReadBasePanel");
+        configureRead();
         populateFieldsFromEntity(entity);
-        if (enableClone()) {
-            configureClone();
-        }
     }
 
     protected void initReadComposite(String id, String className, final ConstantsWithLookup constants) {
         this.entityId = id;
+        configureBack();
         init(className, true, constants);
-        entityCaptionPanel.addStyleName("y-gwt-ReadEntityCaptionPanel");
-        entityFieldsPanel.addStyleName("y-gwt-ReadEntityDisplayWidget");
-        basePanel.addStyleName("y-gwt-ReadBasePanel");
+        configureRead();
         loadEntity(entityId);
-        if (enableClone()) {
-            configureClone();
-        }
     }
 
     protected void initReadComposite(String className, final ConstantsWithLookup constants) {
+        configureBack();
         init(className, true, constants);
+        configureRead();
+        loadEntity(null);
+    }
+
+    protected void configureRead() {
         entityCaptionPanel.addStyleName("y-gwt-ReadEntityCaptionPanel");
         entityFieldsPanel.addStyleName("y-gwt-ReadEntityDisplayWidget");
         basePanel.addStyleName("y-gwt-ReadBasePanel");
-        loadEntity(null);
         if (enableClone()) {
             configureClone();
         }
@@ -63,6 +64,30 @@ public abstract class TReadComposite extends TCRUDComposite {
                 cloneClicked();
             }
         });
+    }
+
+    protected void configureBack() {
+        if (enableBack()) {
+            entityPanel.add(backL);
+            backL.addClickHandler(new ClickHandler() {
+                @Override
+                public void onClick(ClickEvent event) {
+                    if (getReadAllPanel() != null) {
+                        Widget parent = TReadComposite.this.getParent();
+                        TReadComposite.this.removeFromParent();
+                        ((Panel) parent).add(getReadAllPanel());
+                    }
+                }
+            });
+        }
+    }
+
+    protected boolean enableBack() {
+        return false;
+    }
+
+    protected ReadAllComposite getReadAllPanel() {
+        return null;
     }
 
     protected boolean enableClone() {
