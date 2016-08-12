b02af1627e9f2ea2c8cea72b3e9872e659f68a04
Sudharani bandaru <sudharani.bandaru@SSTECHLAPTOP362.sstech.internal>
2016-08-12 15:24:16 -0400

    Create CRUD operations for Release Notes.

---
+++ chili-master/chili-commons/src/main/java/info/chili/notifications/ReleaseNotesDao.java
@@ -0,0 +1,57 @@
+/**
+ * System Soft Technologies Copyright (C) 2013 ayalamanchili@sstech.mobi
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.chili.notifications;
+
+import info.chili.notifications.ReleaseNotes;
+import info.chili.spring.SpringContext;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ *
+ * @author sudharani.bandaru
+ */
+@Repository("releaseNotesDao")
+@Scope("prototype")
+public class ReleaseNotesDao {
+
+    @Autowired
+    protected MongoOperations mongoTemplate;
+  
+    public void save(ReleaseNotes entity) {
+        mongoTemplate.save(entity);
+    }
+    
+    public ReleaseNotes findById(String userId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("userIds").is(userId.trim()));
+        return mongoTemplate.findOne(query, ReleaseNotes.class);
+    }
+
+    public void delete(String userId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("userIds").is(userId.trim()));
+        mongoTemplate.remove(mongoTemplate.findOne(query, ReleaseNotes.class));
+    }
+    
+    public List<ReleaseNotes> query() {
+        Query query = new Query();
+        return mongoTemplate.find(query, ReleaseNotes.class);
+    }
+    public static ReleaseNotesDao instance() {
+        return (ReleaseNotesDao) SpringContext.getBean("releaseNotesDao");
+    }
+
+}
